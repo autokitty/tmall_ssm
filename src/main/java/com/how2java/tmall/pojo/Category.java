26be286743ddec069ef4031b10074ab07bd795b4
autokitty <631649236@qq.com>
2020-03-24 16:59:46 +0800

    test

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -1,14 +1,14 @@
 package com.how2java.tmall.pojo;
 
 public class Category {
-    private Integer id;
+    private int id;
     private String name;
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -17,6 +17,6 @@ public class Category {
     }
 
     public void setName(String name) {
-        this.name=name==null?null:name.trim();
+        this.name = name==null?null:name.trim();
     }
 }
--- src/main/webapp/WEB-INF/jsp/admin/listCategory.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/listCategory.jsp
@@ -6,17 +6,15 @@
 <%@include file="../include/admin/adminNavigator.jsp"%>
 
 <script>
-    $(function(){
-
-        $("#addForm").submit(function(){
-            if(!checkEmpty("name","分类名称"))
+    $(function () {
+        $("#addFrom").submit(function () {
+            if(!checkEmpty("name","分类图片"))
                 return false;
             if(!checkEmpty("categoryPic","分类图片"))
                 return false;
             return true;
-        });
-    });
-
+        })
+    })
 </script>
 
 <title>分类管理</title>
